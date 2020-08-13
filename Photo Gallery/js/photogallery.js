c2b8898fea47f07adcde3b5cffd8bc447040a1e8
Shahriar Ferdous <SFOcean@users.noreply.github.com>
2020-08-13 15:39:59 +0600

    assignment week 2 create

---
+++ Photo Gallery/js/photogallery.js
@@ -0,0 +1,23 @@
+function upDate(element){
+
+    /*2) Change the text  of the div with the id = "image" 
+    to the alt text of the preview image */
+    document.getElementById('image').innerHTML = element.alt;
+
+    /*1) change the url for the background image of the div with the id = "image" 
+    to the source file of the preview image*/
+    img = element.src;
+    document.getElementById('image').style.backgroundImage = "url('" +img+ "')";
+}
+
+function unDo(){
+
+    /*2) Change the text  of the div with the id = "image" 
+    back to the original text.  You can use the html code to see what that original text was
+    */
+    document.getElementById('image').innerHTML = 'Hover over an imgae';
+
+    /*1) Update the url for the background image of the div with the id = "image" 
+    back to the orginal-image.  You can use the css code to see what that original URL was*/
+    document.getElementById('image').style.backgroundImage = "url('')";
+}
---
+++ This/this.js
@@ -0,0 +1,10 @@
+
+
+function upDate(element){
+    document.getElementById('image').innerHTML = element.alt;
+
+}
+
+function unDo(){
+    document.getElementById('image').innerHTML = 'Hover over an imgae';
+}
